e2419b92225a83ff832c36f67e79cb47d43d9099
MochiXu <mochix@moqi.ai>
2022-05-24 16:56:36 +0800

    dev02 third commit

--- src/test.java
+++ src/test.java
@@ -3,7 +3,9 @@ import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 public class test {
     public static void main(String args[]){
         //This is master
+        System.out.println("dev02 upper");
         //dev01Change
         System.out.println("dev02 add");
+        System.out.println("dev02 down");
     }
 }
