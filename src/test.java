476ddfdcb17d8ac5b60b07e21916382f8c56fddb
MochiXu <mochix@moqi.ai>
2022-05-24 16:40:13 +0800

    dev02 first commit

--- src/test.java
+++ src/test.java
@@ -1,6 +1,9 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 public class test {
     public static void main(String args[]){
         //This is master
         //dev01Change
+        System.out.println("dev02 add");
     }
 }
