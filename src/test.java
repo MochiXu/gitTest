d9373dfb5fa0ff8ae3883d3a1e6bf67ce184cae4
MochiXu <mochix@moqi.ai>
2022-05-24 16:49:42 +0800

    dev01 second commit

--- src/test.java
+++ src/test.java
@@ -2,5 +2,8 @@ public class test {
     public static void main(String args[]){
         //This is master
         //dev01Change
+        int a=3;
+        int b=4;
+        System.out.println(a+b);
     }
 }
