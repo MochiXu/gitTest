9344fd9e95bd4a5fa30607400cbfb35a3d408068
MochiXu <mochix@moqi.ai>
2022-06-05 15:31:35 +0800

    mcdev2

--- src/test.java
+++ src/test.java
@@ -5,14 +5,29 @@ public class test {
         //This is master
         System.out.println("dev02 upper");
         //dev01Change
-        int a=3;
-        int b=4;
-        System.out.println(a+b);
+        int a = 3;
+        int b = 4;
+        System.out.println(a + b);
         //mochix/mcdev01-begin
-        String io="ds";
-        String mochi2="modified--mochixDEV2";
+        String io = "ds";
+        String mochi2 = "modified--mochixDEV2";
         //mochix/mcdev01-end
         System.out.println("dev02 add");
         System.out.println("dev02 down");
+        for (int k = 0; k > 0; k--) {
+            System.out.println(k);
+        }
+        for (int i = 0; i < 9; i++) {
+            System.out.println(i + " -");
+        }
+        for (int k = 0; k < 20; k++) {
+            System.out.println(k);
+        }
+        for (int k = 0; k < 20; k++) {
+            System.out.println(k);
+        }
+        for (int i = 9; i < 20; i--) {
+            System.out.println("mo");
+        }
     }
 }
